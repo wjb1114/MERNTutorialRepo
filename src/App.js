97db6ee213252b53d7a2850d016f48e425018724
wjb1114 <kbd1114@gmail.com>
2019-10-15 11:12:15 -0500

    Implemented basic form for creating todo, outputs to console for now.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import CreateTodo from "./components/create-todo.component";
+import EditTodo from "./components/edit-todo.component";
+import TodosList from "./components/todos-list.component";
+
+import logo from "./logo.jpg";
+
 class App extends Component {
-	rendeer() {
+	render() {
 		return (
 			<Router>
-				<Route path="/" exact component={TodosList} />
-				<Route path="/edit/:id" component={EditTodo} />
-				<Route path="/create" component={CreateTodo} />
 				<div className="container">
-					<h2>MERN-Stack Todo App</h2>
+					<nav className="navbar navbar-expand-lg navbar-light bg-light">
+						<a className="navbar-brand" href="https://wjb1114.github.io" target="_blank" rel="noopener noreferrer">
+							<img src={logo} width="30" height="30" alt="wjb1114.github.io" />
+						</a>
+						<Link to="/" className="navbar-brand">MERN-Stack Todo App</Link>
+						<div className="collapse navbar-collapse">
+							<ul className="navbar-nav mr-auto">
+								<li className="navbar-item">
+									<Link to="/" className="nav-link">Todos</Link>
+								</li>
+								<li className="navbar-item">
+									<Link to="/create" className="nav-link">Create Todo</Link>
+								</li>
+							</ul>
+						</div>
+					</nav>
+					<br />
+					<Route path="/" exact component={TodosList} />
+					<Route path="/edit/:id" component={EditTodo} />
+					<Route path="/create" component={CreateTodo} />
 				</div>
 			</Router>
 		);
